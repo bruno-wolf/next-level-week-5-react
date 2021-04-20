68bf43fbee5da07f3e02b9f298d51a9bbbbe673a
theycallmewolf <bruno.m.lobato@gmail.com>
2021-04-20 18:49:05 +0100

    v.0.0.1 adds header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import format from 'date-fns/format';
+import pt from 'date-fns/locale/pt'
+import styles from './styles.module.scss';
+
+export function Header() {
+
+  const currentDate = format(new Date(), 'EEEEEE, d MMM', {
+    locale : pt,
+  });
+
+  return (
+    <header className={ styles.headerContainer }>
+      <img src="/logo.svg" alt="Podcastr"/>
+      <p>O melhor para ouvires. Sempre!</p>
+      <span>{ currentDate }</span>
+    </header>
+  );
+}
