b398551864f32a82819aee36df03bebd0c5b3eb9
theycallmewolf <bruno.m.lobato@gmail.com>
2021-04-20 20:11:27 +0100

    v.0.0.1 get episodes as static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+export default function Home(props) {
+  console.log(props.episodes);
+
   return (
     <>
       <h1>Hello Wolf @ next!</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 8 * 60 * 60 // 8 horas
+  };
 }
