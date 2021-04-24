adaea18c317f147d8bca1939b08aab36583a81be
theycallmewolf <bruno.m.lobato@gmail.com>
2021-04-24 10:08:24 +0100

    v.0.0.1 controls play / pause

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,17 +1,31 @@
 import Image from 'next/image';
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import Slider from 'rc-slider';
 import { PlayerContext } from '../../contexts/PlayerContext';
 import 'rc-slider/assets/index.css'
 import styles from './styles.module.scss';
 
 export function Player() {
+  const audioRef = useRef<HTMLAudioElement>(null); 
+  // first sets to null then adds the ref on element loading <audio ref={audioRef}
+  // all HTML elements are globally available for TypeScript
+
   const { 
     episodeList,
     currentEpisodeIndex,
     isPlaying,
     togglePlay,
+    setPlayingState,
   } = useContext(PlayerContext);
+
+  useEffect(() => {
+    if(!audioRef.current) return; // audioRef.current -> current value
+    
+    isPlaying
+      ? audioRef.current.play()
+      : audioRef.current.pause();
+      
+  }, [isPlaying]);
 
   const episode = episodeList[Number(currentEpisodeIndex)];
 
@@ -57,7 +71,13 @@ export function Player() {
         </div>
         
         { episode && (
-          <audio src={ episode.url } autoPlay />
+          <audio 
+            ref={audioRef}
+            src={ episode.url }
+            autoPlay
+            onPlay={() => setPlayingState(true)}
+            onPause={() => setPlayingState(false)}
+          />
         )}
 
         <div className={ styles.buttons }>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,8 +22,12 @@ function MyApp({ Component, pageProps }) {
     setIsPlaying(!isPlaying);
   }
 
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state)
+  }
+
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, isPlaying, togglePlay, play }}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, isPlaying, togglePlay, play, setPlayingState }}>
       <div className={ styles.wrapper }>
         <main>
           <Header />
